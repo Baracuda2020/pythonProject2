32e346c28c5c3b0546cd9dbacbf4a2cbfa0a0fa7
Baracuda2020 <rustamaslanov2020@gmail.com>
2024-11-18 20:26:16 +0300

    Add files via upload

---
+++ module_2_2+.py
@@ -0,0 +1,11 @@
+# Все ли равны
+first = 24
+second = 36
+third = 55
+if first == second == third:
+    print(3)
+elif first == second or first == third or second == third:
+    print(2)
+else:
+    print(0)
+
