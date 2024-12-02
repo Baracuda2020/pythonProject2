e372ee9ab0b14664507b780354c95b29e582962f
Baracuda2020 <rustamaslanov2020@gmail.com>
2024-12-02 20:09:09 +0300

    Add files via upload

--- module _3_5.py
+++ module _3_5.py
@@ -3,10 +3,10 @@
 
 def single_root_words(root_word, *other_words):
     same_words = []
-    i = root_word.lower()
-    for i in other_words:
-        if root_word in i or i in root_word:
-            same_words.append(i)
+    root_word = root_word.lower()
+    for word in other_words:
+        if root_word in word.lower() or word.lower() in root_word:
+            same_words.append(word)
             return same_words
 
 result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
